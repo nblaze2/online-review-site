30295cbb3b5467bc8f8d0cd10fac0cf4ca22acd3
Nick Blaisdell <nick.blaisdell@gmail.com>
2017-01-13 16:51:26 -0500

    first, almost finished first story

---
+++ spec/features/user_registration_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'user creates an account', %q(
+
+  As a prospective user
+  I want to create an account
+  So that I can post items and review them
+
+) do
+
+  scenario 'successfully registers an account' do
+    visit '/'
+    click_on 'Sign up'
+    fill_in 'Email', with: 'test@dummy.com'
+    fill_in 'Password', with: '1234abcd'
+    fill_in 'Password confirmation', with: '1234abcd'
+    click_on 'Create Account'
+
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content('Edit account')
+  end
+end
