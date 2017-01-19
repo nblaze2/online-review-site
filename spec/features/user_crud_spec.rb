aff7a973b7836ac62f9f28b36c80cb072272823e
Nick Blaisdell <nick.blaisdell@gmail.com>
2017-01-19 11:15:35 -0500

    all item crud stories done. tests passing. sad stories? crud review stories next

--- spec/features/user_crud_spec.rb
+++ spec/features/user_crud_spec.rb
@@ -69,3 +69,55 @@ feature 'authenticated user views movie information', %q(
     expect(page).to have_content('Post a Review')
   end
 end
+
+feature 'authenticated user updates a movies information', %q(
+
+  As an authenticated user
+  I want to update an item's information
+  So that I can correct errors or provide new information
+
+  ) do
+
+  scenario 'successfully updates details of a movie' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    first_movie = FactoryGirl.create(:movie, user_id: user.id)
+
+    visit '/'
+
+    click_on 'Details/Reviews'
+    click_on 'Edit'
+    fill_in 'Title', with: 'Die Hard'
+    fill_in 'Year', with: '1988'
+    click_on 'Update Movie'
+    expect(page).to have_content('Die Hard')
+    expect(page).to have_content('1988')
+    expect(page).to have_content('Post a Review')
+  end
+end
+
+feature 'authenticated user deletes a movie and its reviews', %q(
+
+  As an authenticated user
+  I want to delete an item
+  So that no one can review it
+
+  ) do
+
+  scenario 'successfully deletes a movie' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    first_movie = FactoryGirl.create(:movie, title: 'Die Hard 2', year: '1990', user_id: user.id)
+
+    visit '/'
+
+    click_on 'Details/Reviews'
+    expect(page).to have_content('Die Hard 2')
+    expect(page).to have_content('1990')
+    click_on 'Destroy'
+
+    expect(page).to have_content('Movie was successfully destroyed.')
+    expect(page).to_not have_content('Die Hard 2')
+    expect(page).to_not have_content('1990')
+  end
+end
